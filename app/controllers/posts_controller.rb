fbc18291381185efdc0a5a51795178b040f894be
Jaaystones <Joelpaul345@gmail.com>
2024-02-15 01:04:10 +0100

    :bulb: Tailwindcss implementation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,37 +1,51 @@
 class PostsController < ApplicationController
-    # app/controllers/posts_controller.rb
-    before_action :authenticate_user!, only: [:new, :create]
-    
-    def index
-      @posts = Post.all
-    end
-  
-    def new
-      @post = current_user.posts.build
-    end
-  
-    def create
-      @post = current_user.posts.build(post_params)
-      if @post.save
-        redirect_to posts_path, notice: 'Post was successfully created.'
-      else
-        render :new
-      end
-    end
+  before_action :authenticate_user!, only: [:new, :create, :destroy]
+  before_action :set_post, only: [:edit, :update, :destroy]
 
-    def edit
-        @post = Post.find_by(id: params[:id])
-        if @post.nil?
-          flash[:alert] = "Post not found or you don't have permission to edit it."
-          redirect_to posts_path
-        end
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = current_user.posts.build
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to posts_path, notice: 'Post was successfully created.'
+    else
+      render :new
     end
-    private
-  
-    def post_params
-      params.require(:post).permit(:title, :content)
+  end
+
+  def edit
+    if @post.nil?
+      flash[:alert] = "Post not found or you don't have permission to edit it."
+      redirect_to posts_path
     end
-    
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to posts_path, notice: 'Post was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice: 'Post was successfully deleted.'
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
+  def set_post
+    @post = Post.find_by(id: params[:id])
+  end
 end
-  
-
